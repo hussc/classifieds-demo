4ec56643f262fc0e6e0a39c9a7ce2ea92c694773
Hussein AlRyalat <hussc.alryalat@gmail.com>
2022-07-03 19:49:04 +0300

    First project setup

---
+++ classifieds/classifiedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  classifiedsApp.swift
+//  classifieds
+//
+//  Created by Hussein on 03/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct classifiedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
